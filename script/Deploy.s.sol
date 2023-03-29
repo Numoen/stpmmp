bcfe76bf599c0c5ca892696e1556a6f93e05368b
Kyle Scott <kyscott@umich.edu>
2023-03-29 12:44:14 -0400

    deploy liquid staking

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -13,26 +13,29 @@ import { LendgineRouter } from "src/periphery/LendgineRouter.sol";
 contract Deploy is Script {
   address constant create3Factory = 0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1;
 
-  address constant uniV2Factory = 0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32;
+  address constant uniV2Factory = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4;
   address constant uniV3Factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
 
-  address constant weth = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+  address constant weth = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+  address constant liquidStaking = 0x5979D7b546E38E414F7E9822514be443A4800529;
 
-  function run() external returns (address factory, address liquidityManager, address lendgineRouter) {
+  function run() external returns (address factory, address liquidityManager, address lendgineRouter, address lendgine) {
     CREATE3Factory create3 = CREATE3Factory(create3Factory);
 
     uint256 pk = vm.envUint("PRIVATE_KEY");
     vm.startBroadcast(pk);
 
-    factory = create3.deploy(keccak256("NumoenFactory"), type(Factory).creationCode);
+    factory = create3.deploy(keccak256("NumoenFactoryLS"), type(Factory).creationCode);
 
     liquidityManager = create3.deploy(
-      keccak256("NumoenLiquidityManager"), bytes.concat(type(LiquidityManager).creationCode, abi.encode(factory, weth))
+      keccak256("NumoenLiquidityManagerLS"), bytes.concat(type(LiquidityManager).creationCode, abi.encode(factory, weth))
     );
 
     lendgineRouter = create3.deploy(
-      keccak256("NumoenLendgineRouter"),
+      keccak256("NumoenLendgineRouterLS"),
       bytes.concat(type(LendgineRouter).creationCode, abi.encode(factory, uniV2Factory, uniV3Factory, weth))
     );
+
+    lendgine = Factory(factory).createLendgine(weth, liquidStaking, 18, 18, 1.6e18);
   }
 }
